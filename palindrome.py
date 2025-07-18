da04958ac90f58cc3b63347d988e739d09be8028
Kalpana2606 <165750067+Kalpana2606@users.noreply.github.com>
2025-07-18 15:21:13 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,35 @@
+class Node:
+  
+  def __init__(self,data):
+   
+     self.data = data
+  
+     self.next = None
+  
+  def is_palindrome(head):
+ 
+       s=""
+      
+       while head:
+      
+           s += head.data
+   
+           head = head.next
+  
+       return s == s[::-1]
+
+head = Node('M')
+
+head.next = Node('O')
+
+head.next.next = Node('M')
+
+
+if Node.is_palindrome(head):
+  
+   print("palindrome")
+
+else:
+  
+     print("Not palindrome")
+
---
+++ subset.py
@@ -0,0 +1,19 @@
+num=[1,2,3,4]
+
+subsets=[[]]
+
+for num in num:
+   
+    new_subsets=[]
+  
+    for A in subsets:
+   
+        new_subsets.append(A+[num])
+ 
+    subsets+=new_subsets
+
+for A in subsets:
+    
+    if sum(A)%2==0:
+       
+        print(A)
